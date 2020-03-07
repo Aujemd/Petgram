83ceb5e1bde2ec7a8c21697ad9b4fa4a8e68d07f
Johan Mora <joenmodu99@gmail.com>
2020-03-07 19:56:55 -0400

    Usar render Props para recuperar una foto

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import {ListOfCategories} from './components/ListOfCategories/index'
 import {GlobalStyle} from './styles/GlobalStyles'
 import {ListOfPhotoCards} from './container/ListOfPhotoCards'
 import {Logo} from './components/Logo/index'
+import {PhotoCardWithQuery} from './container/PhotoCardWithQuery'
 
-export const App = () => (
+export const App = () => {
+
+    const urlParams = new window.URLSearchParams(window.location.search)
+    const detailId = urlParams.get('detail')
+    console.log(detailId)
+    
+    return(    
     <>
-    <GlobalStyle/>
-    <Logo/>
-    <ListOfCategories/>
-    <ListOfPhotoCards categoryId = {1}></ListOfPhotoCards>
+        <GlobalStyle/>
+        <Logo/>
+        {
+            detailId
+            ? <PhotoCardWithQuery id = {detailId}/>
+            :
+            <>
+            <ListOfCategories/>
+            <ListOfPhotoCards categoryId = {1}></ListOfPhotoCards>
+            </>
+        }
     </>
-)
+    )
+}
+
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -18,7 +18,7 @@ export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
         <Article ref = {ref}>
             {
                 show && <>
-                <a href={`/detail/${id}`}>
+                <a href={`/?detail=${id}`}>
                     <ImgWrapper>
                         <Img src={src}></Img>
                     </ImgWrapper>
