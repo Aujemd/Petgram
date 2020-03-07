f61afe80008040b8a1f2f7b399b9554be64edb6f
Johan Mora <joenmodu99@gmail.com>
2020-03-06 22:16:07 -0400

    ¿Qué son los Hooks?

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Category} from '../Category'
 import {List, Item} from './styles'
 
-import {categories} from '../../../api/db.json'
-
 export const ListOfCategories = () => {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        window.fetch('https://petgram-server-hksev77it.now.sh/categories')
+        .then(res => res.json())
+        .then(response => {
+            setCategories(response)
+        })
+    }, []) //RECORDAR EL ARRAY VACIO PORQUE SINO SE EJECUTARA INFINITAMENTE
     return (
         <List>
             {
