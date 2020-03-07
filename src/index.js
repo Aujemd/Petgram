0f614d2a8f56241baff8f29f3203703a2b146d51
Johan Mora <joenmodu99@gmail.com>
2020-03-07 17:57:57 -0400

    ¿Qué es GraphQl y React Apollo Inicializando

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import {ApolloProvider} from 'react-apollo'
 import {App} from './App'
 
-ReactDOM.render(<App/>, document.getElementById('app'))
+const cliente = new ApolloClient({
+    uri: 'https://petgram-server.jemd.now.sh/graphql'
+})
+
+ReactDOM.render(
+<ApolloProvider client = {cliente}>
+    <App/>
+</ApolloProvider> 
+, document.getElementById('app'))
