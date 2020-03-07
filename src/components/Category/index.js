516d9c0f76a8333f88a06f5ee05acd26d582ab6c
Johan Mora <joenmodu99@gmail.com>
2020-03-07 11:58:21 -0400

    useCategoriesData

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Anchor, Image} from './styles'
 import DEFAULT_IMAGE from '../../assets/cat.jpg'
 
-export const Category = ({cover, path, emoji}) => (
+export const Category = ({cover = DEFAULT_IMAGE, path, emoji = '🐢'}) => (
     <Anchor href = {path}>
         <Image src ={cover}/>
         {emoji}
--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,18 +2,28 @@ import React, {useEffect, useState} from 'react'
 import {Category} from '../Category'
 import {List, Item} from './styles'
 
-export const ListOfCategories = () => {
-
+function useCategoriesData(){ //Custom Hook
     const [categories, setCategories] = useState([])
-    const [showFixed, setShowFixed] = useState(false)
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+        setLoading(true)
         window.fetch('https://petgram-server-hksev77it.now.sh/categories')
         .then(res => res.json())
         .then(response => {
             setCategories(response)
+            setLoading(false)
         })
     }, []) //RECORDAR EL ARRAY VACIO PORQUE SINO SE EJECUTARA INFINITAMENTE
+
+    return {categories, loading}
+}
+
+export const ListOfCategories = () => {
+
+    const {categories, loading} = useCategoriesData()
+
+    const [showFixed, setShowFixed] = useState(false)
     
     useEffect(() => {
         const onScroll = e => {
@@ -28,9 +38,10 @@ export const ListOfCategories = () => {
     }, [showFixed])
 
     const renderList = (fixed) => (
-        <List className = {fixed ? 'fixed' : ''}>
+        <List fixed={fixed}>
             {
-                categories.map(category => <Item key = {category.id}><Category {...category}/></Item>)
+                loading ? <Item key = 'loading'><Category></Category></Item>
+                : categories.map(category => <Item key = {category.id}><Category {...category}/></Item>)
             }
         </List>
     )
