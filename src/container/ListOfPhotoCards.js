156c4581ef4baf645e746daf9c303be9c7faa4fc
Johan Mora <joenmodu99@gmail.com>
2020-03-07 18:28:53 -0400

    Parámetros para un query con GraphQL

---
+++ src/container/ListOfPhotoCards.js
@@ -0,0 +1,4 @@
+import {withPhotos} from '../hoc/withPhotos'
+import {ListOfPhotoCardsComponent}  from '../components/ListOfPhotoCards/index'
+
+export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
