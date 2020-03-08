2bc98cd0a26bcb5a268943d6018d26c73d0403a3
Johan Mora <joenmodu99@gmail.com>
2020-03-07 20:08:32 -0400

    Refactorizando

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ import {PhotoCardWithQuery} from './container/PhotoCardWithQuery'
 export const App = () => {
 
     const urlParams = new window.URLSearchParams(window.location.search)
-    const detailId = urlParams.get('detail')
-    console.log(detailId)
-    
+    const detailId = urlParams.get('detail')    
     return(    
     <>
         <GlobalStyle/>
--- src/container/PhotoCardWithQuery.js
+++ src/container/PhotoCardWithQuery.js
@@ -4,7 +4,7 @@ import {PhotoCard} from '../components/PhotoCard'
 import {gql} from 'apollo-boost'
 import {Query} from 'react-apollo'
 
-const query = gql`
+const GET_SINGLE_PHOTO = gql`
     query getSinglePhoto($id:ID!){
         photo(id:$id){
             id
@@ -17,15 +17,17 @@ const query = gql`
     }
 `
 
+const renderProp = ({loading, error, data}) => {
+    if(loading) return <p>Loading ...</p>
+    if(error) return <p>Error!</p>
+    
+    const {photo = {}} = data
+    return <PhotoCard {...photo}/>
+}
+
 export const PhotoCardWithQuery = ({id}) => (
-    <Query query = {query} variables = {{id}}>
-        {
-            ({loading, error, data}) => {
-                if(loading) return null
-                const {photo = {}} = data
-                return <PhotoCard {...photo}/>
-            }
-        }
+    <Query query = {GET_SINGLE_PHOTO} variables = {{id}}>
+        {renderProp}
     </Query>
     
 )
