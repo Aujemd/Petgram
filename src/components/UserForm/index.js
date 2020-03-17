3a69b5f895718b71d9441533ad548907430c8a72
Johan Mora <joenmodu99@gmail.com>
2020-03-17 16:09:55 -0400

    Estilando el formulario

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
 
 import {useInputValue} from '../../hooks/useInputValue'
+import {Form, Input, Button, Title} from './styles'
 
-export const UserForm = ({onSubmit}) => {
+export const UserForm = ({onSubmit, title}) => {
 
     const email = useInputValue('')
     const password = useInputValue('')  
 
-    return <form onSubmit={onSubmit}>
-        <input placeholder='Email' {...email}></input>
-        <input placeholder='Password' type='password' {...password}></input>
-        <button>Iniciar Sesión</button>
-    </form>
+    return (
+        <>
+        <Title>{title}</Title>
+        <Form onSubmit={onSubmit}>
+        <Input placeholder='Email' {...email}></Input>
+        <Input placeholder='Password' type='password' {...password}></Input>
+        <Button>{title}</Button>
+        </Form>
+        </>
+    )
 }
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -5,7 +5,10 @@ export const NotRegisteredUser = () =>(
     <Context.Consumer>
         {
             ({activateAuth}) =>{
-                return <UserForm onSubmit={activateAuth} />
+                return <>
+                <UserForm title='Registrarse' onSubmit={activateAuth} />
+                <UserForm title='Iniciar Sesión' onSubmit={activateAuth} />
+                </>
             }
         }
     </Context.Consumer>
