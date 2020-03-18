678a722ca508d0105663ebe16af4fa934f651d7e
Johan Mora <joenmodu99@gmail.com>
2020-03-18 16:26:13 -0400

    Últimos retoques a las rutas de nuestra aplicación

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {GlobalStyle} from './styles/GlobalStyles'
 import {Logo} from './components/Logo/index'
 import {Home} from './pages/Home'
-import {Router} from '@reach/router'
+import {Router, Redirect} from '@reach/router'
 import {Detail} from './pages/Detail'
 import {Favs} from './pages/Favs'
 import {User} from './pages/User'
 import {NotRegisteredUser} from './pages/NotRegisteredUser'
 import {NavBar} from './components/NavBar'
-import Context from './Context'
+import {Context} from './Context'
+import {NotFound} from './pages/NotFound'
 
-export const App = () => {   
+export const App = () => {
+    
+    const {isAuth} = useContext(Context)
     return(    
     <>
         <GlobalStyle/>
         <Logo/>
         <Router>
+            <NotFound default />
             <Home path='/'/>
             <Home path='/pet/:id'/>
             <Detail path='/detail/:detailId'/>
+            {!isAuth && <NotRegisteredUser path='/login'/>}
+            {!isAuth && <Redirect from='/favs' to='/login' noThrow/>}
+            {!isAuth && <Redirect from='/user' to='/login' noThrow/>}
+            {isAuth && <Redirect from='/login' to = '/'/>}
+            <Favs path='/favs'/>
+            <User path='/user'/>
         </Router>
-        <Context.Consumer>
-            {
-                ({isAuth}) => 
-                isAuth ? 
-                <Router>
-                    <Favs path='/favs'/>
-                    <User path='/user'/>
-                </Router>
-                :                
-                <Router>
-                    <NotRegisteredUser path='/favs'></NotRegisteredUser>
-                    <NotRegisteredUser path='/user'></NotRegisteredUser>
-                </Router>
-            }
-        </Context.Consumer>
+
         <NavBar></NavBar>
     </>
     )
