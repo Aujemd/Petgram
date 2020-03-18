d1cca0fc9f8b6cb93884bc85ab4f68b9edd24292
Johan Mora <joenmodu99@gmail.com>
2020-03-18 13:10:45 -0400

    Dar like como usuario registrado

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -5,15 +5,12 @@ const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_8
 import {FavButton} from '../FavButton/'
 
 import {ImgWrapper, Img, Article} from './styles'
-import {useLocalStorage} from '../../hooks/useLocalStorage'
 import {useNearScreen} from '../../hooks/useNearScreen'
 import {ToggleLikeMutation} from '../../container/ToggleLikeMutation'
 import {Link} from '@reach/router'
-export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
+export const PhotoCard = ({id, liked, likes = 0, src = DEFAULT_IMAGE}) => {
     
     const[show, ref] = useNearScreen()
-    const key = `like-${id}`
-    const [liked, setLiked] = useLocalStorage(key, false)
     
     return (
         <Article ref = {ref}>
@@ -28,10 +25,9 @@ export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
                     {
                         (toggleLike) => {
                             const handleFavClick = () => {
-                                !liked && toggleLike({variables: {
+                                toggleLike({variables: {
                                     input:{id}
                                 }})
-                                setLiked(!liked)
                             }
                             
                             return <FavButton liked={liked} likes={likes} onClick={handleFavClick}></FavButton>
--- src/index.js
+++ src/index.js
@@ -6,7 +6,24 @@ import {App} from './App'
 import Context from './Context'
 
 const cliente = new ApolloClient({
-    uri: 'https://petgram-server.jemd.now.sh/graphql'
+    uri: 'https://petgram-server.jemd.now.sh/graphql',
+    request: operation => {
+        const token = window.sessionStorage.getItem('token')
+        const authorization = token ? `Bearer ${token}` : ''
+        operation.setContext({
+            headers:{
+                authorization
+            }
+        })
+    },
+    onError: error => {
+        const {networkError} = error
+        if(networkError && networkError.code === "invalid_token"){
+            window.sessionStorage.removeItem('token')
+            window.location.href = '/'
+        }
+    } 
+
 })
 
 ReactDOM.render(
